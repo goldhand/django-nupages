68c64a57996d14a653abfe3fdecbc76fafa40b6d
Will Farley <will@django.nu>
2014-03-11 16:03:15 -0600

    fixes urls finally

--- nupages/models.py
+++ nupages/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
 
 from django_extensions.db.models import TimeStampedModel, TitleSlugDescriptionModel, ActivatorModel
 
@@ -24,6 +25,9 @@ class Page(TimeStampedModel, TitleSlugDescriptionModel, ActivatorModel):
 		verbose_name = _('Page')
 		verbose_name_plural = _('Pages')
 		ordering = ('-created',)
+
+	def __unicode__(self):
+		return u'%s' % self.title
 
 	def get_absolute_url(self):
 		return reverse('nupages:detail', kwargs={'slug': self.slug})
